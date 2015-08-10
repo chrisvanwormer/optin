2e129ec6d5a6c739eb257817ff74f2596ace3207
Chris VanWormer <chris.vanwormer@clickitdigital.com>
2015-08-10 18:39:14 -0400

    add js functionality for clicks

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('#sendOptIn').on('click', function() {
+        // grab form field data
+        var formName    = $('#name').val();
+        var formEmail    = $('#email').val();
+        var formdata    = 'name=' + formName + '&email=' + formEmail;
+        $('#content-thank-you').foundation('reveal', 'open', {
+            url: 'http://iamfearlesslyme.com/optin/php/app.php?script=optin',
+            type: 'POST',
+            data: formdata,
+            success: function(data) {
+                //$('#messageName').val('');
+                //$('#messageEmail').val('');
+                //$('#messageMessage').val('');
+            },
+            error: function(data) {
+
+                var html = '<h4 class="clickit-section-centered-text">We\'re sorry. Something didn\'t quite go right.  Please try again.</h4><a class="close-reveal-modal">&#215;</a>';
+                $('#content-thank-you').html(html);
+                $('#content-thank-you').foundation('reveal', 'open');
+            }
+        });
+        return false;
+    });
+});
+
+$(document).foundation();
